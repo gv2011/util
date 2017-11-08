1f2b79a712d59abc38f67641d9bbc4fc425c83b3
Vinz <gv2011@users.noreply.github.com>
2017-11-08 09:23:16 +0100

    update license

--- test/src/main/java/com/github/gv2011/testutil/AbstractTest.java
+++ test/src/main/java/com/github/gv2011/testutil/AbstractTest.java
@@ -1,6 +1,30 @@
 package com.github.gv2011.testutil;
 
-import static com.github.gv2011.util.ServiceLoaderUtils.loadService;
+/*-
+ * %---license-start---
+ * The MIT License (MIT)
+ * %
+ * Copyright (C) 2016 - 2017 Vinz (https://github.com/gv2011)
+ * %
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * %---license-end---
+ */
 import static com.github.gv2011.util.StreamUtils.readText;
 import static com.github.gv2011.util.bytes.ByteUtils.copyFromStream;
 import static com.github.gv2011.util.ex.Exceptions.format;
@@ -15,8 +39,6 @@ import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.slf4j.Logger;
 
-import com.github.gv2011.jsoncore.JsonFactory;
-import com.github.gv2011.jsoncore.JsonNode;
 import com.github.gv2011.util.ResourceUtils;
 import com.github.gv2011.util.bytes.ByteUtils;
 import com.github.gv2011.util.bytes.Bytes;
@@ -49,10 +71,6 @@ public abstract class AbstractTest {
 
   protected final String getResourceAsString(final Class<?> base, final String extension){
     return readText(getResource(base, extension)::openStream);
-  }
-
-  protected final JsonNode getResourceAsJson(final String extension){
-    return loadService(JsonFactory.class).newJsonReader().parse(getResource(extension)::openStream);
   }
 
   protected final URL getResource(final String extension){
