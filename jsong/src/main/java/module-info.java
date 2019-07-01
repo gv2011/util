730ca24e437ff3bc8ca5ecd3535019a426bd3faa
Vinz <gv2011@users.noreply.github.com>
2019-07-01 20:24:55 +0200

    modules

--- jsong/src/main/java/module-info.java
+++ jsong/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module com.github.gv2011.jsong{
   requires com.github.gv2011.util.json.imp;
-  requires gson;
+  requires com.github.gv2011.gson;
   provides com.github.gv2011.util.json.imp.Adapter with com.github.gv2011.jsong.JsongAdapter;
 }
