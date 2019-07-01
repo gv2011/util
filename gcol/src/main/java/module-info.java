32c85b2a9cb5b8af81f3b67e221a1f837390bc8b
Vinz <gv2011@users.noreply.github.com>
2019-07-01 18:55:18 +0200

    modules

--- gcol/src/main/java/module-info.java
+++ gcol/src/main/java/module-info.java
@@ -27,6 +27,7 @@ module com.github.gv2011.util.gcol {
   requires com.github.gv2011.util;
   requires com.github.gv2011.guava;
   requires org.slf4j;
+  exports com.github.gv2011.util.gcol;
   provides com.github.gv2011.util.icol.ICollectionFactorySupplier
     with com.github.gv2011.util.gcol.GcolICollectionFactorySupplier;
 }
