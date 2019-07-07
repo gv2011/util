f6107230ffac482cd801be887735f73cdf909cd0
Vinz <gv2011@users.noreply.github.com>
2019-07-07 22:36:47 +0200

    jctrl

--- beans/src/main/java/module-info.java
+++ beans/src/main/java/module-info.java
@@ -1,6 +1,11 @@
 module com.github.gv2011.util.beans.imp{
   requires org.slf4j;
   requires transitive com.github.gv2011.util;
+
+  uses com.github.gv2011.util.json.JsonFactory;
+  uses com.github.gv2011.util.beans.ElementaryTypeHandlerFactory;
+
   exports com.github.gv2011.util.beans.imp;
+
   provides com.github.gv2011.util.beans.TypeRegistry with com.github.gv2011.util.beans.imp.DefaultTypeRegistry;
 }
