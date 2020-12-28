94b21eeb8507be33d6e18f04e95755a309372b14
Vinz <gv2011@users.noreply.github.com>
2020-12-28 14:14:17 +0100

    fixed-build

--- jacksonadapter/src/main/java/module-info.java
+++ jacksonadapter/src/main/java/module-info.java
@@ -1,11 +1,15 @@
 module com.github.gv2011.jacksonadapter{
+  requires transitive com.fasterxml.jackson.databind;
+  requires transitive com.github.gv2011.util.json.imp;
+  
   requires org.slf4j;
   requires com.fasterxml.jackson.core;
-  requires com.fasterxml.jackson.databind;
   requires com.github.gv2011.util.beans.imp;
-  requires com.github.gv2011.util.json.imp;
   
   provides com.fasterxml.jackson.databind.Module with com.github.gv2011.jacksonadapter.ImmutableBeansModule;
   provides com.github.gv2011.util.json.imp.Adapter with com.github.gv2011.jacksonadapter.JacksonAdapter;
   
+  /** For tests only **/
+  exports com.github.gv2011.jacksonadapter to com.github.gv2011.util.beans.imp;
+
 }
