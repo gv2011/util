387f53006564d0b84bdd7f14cec2fd4f9c276a8f
Vinz <gv2011@users.noreply.github.com>
2024-02-05 20:56:34 +0100

    fix-tests

--- test5/src/test/java/com/github/gv2011/tempfile/TempFilesTest.java
+++ test5/src/test/java/com/github/gv2011/tempfile/TempFilesTest.java
@@ -1,5 +1,6 @@
 package com.github.gv2011.tempfile;
 
+import static com.github.gv2011.util.icol.ICollections.single;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -7,6 +8,8 @@ import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
+import com.github.gv2011.util.log.LogAdapter;
+import com.github.gv2011.util.serviceloader.RecursiveServiceLoader;
 import com.github.gv2011.util.tempfile.TempDir;
 import com.github.gv2011.util.tempfile.TempFiles;
 
@@ -14,6 +17,22 @@ class TempFilesTest {
 
   @Test
   void test() {
+    assertThat(
+      Paths.get(".").toAbsolutePath().normalize(),
+      is(Paths.get("/data/src/gv2011/util/test5").toAbsolutePath())
+    );
+    assertThat(
+      RecursiveServiceLoader.service(LogAdapter.class).tryGetLogConfiguration(),
+      is(single(Paths.get("logback.xml").toUri()))
+    );
+    assertThat(
+      Paths.get(".").toAbsolutePath().normalize(),
+      is(Paths.get("/data/src/gv2011/util/test5").toAbsolutePath())
+    );
+    assertThat(
+      ((TempFileFactoryImp)TempFiles.tempFileFactory()).baseDir.get().toAbsolutePath(),
+      is(Paths.get("log", "tmp").toAbsolutePath())
+    );
     try(TempDir tempDir = TempFiles.createTempDir()){
       assertThat(tempDir.path().getParent(), is(Paths.get("log/tmp").toAbsolutePath()));
     }
